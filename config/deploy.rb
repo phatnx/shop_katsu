aa6ca0f9278def86056df366639d9fc73fc6a5a9
phatnx <phatdaorong@gmail.com>
2024-04-12 14:23:43 +0700

    gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,5 @@
 # Change these
 # server '100.26.47.209', port: 22, roles: [:web, :app, :db], primary: true
-require "bundler/capistrano"
 
 server '54.89.240.243', port: 22, roles: [:web, :app, :db], primary: true
 
