98bf2353514126ec1d1001b7e25a895fad74ec5c
phatnx <phatdaorong@gmail.com>
2024-04-12 16:52:58 +0700

    remove channels

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ import * as ActiveStorage from "@rails/activestorage"
 require("trix")
 require("@rails/actiontext")
 // import "../trix-editor-overrides"
-import "channels"
+// import "channels"
 
 Rails.start()
 Turbolinks.start()
